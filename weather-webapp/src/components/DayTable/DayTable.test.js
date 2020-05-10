f4ea1b3e37dbc80d1a7f3f1451c2fa028b595a16
Laura Artunduaga <laura.perea.artunduaga@gmail.com>
2020-05-10 11:25:22 -0400

    test

--- weather-webapp/src/components/DayTable/DayTable.test.js
+++ weather-webapp/src/components/DayTable/DayTable.test.js
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import { configure, shallow }from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import RepoTable from './RepoTable';
-// import { Table } from 'material-ui/Table';
-// import Spinner from '../Spinner';
-// configure({adapter: new Adapter()});
+import React from 'react';
+import { configure, shallow }from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import DayTable from './DayTable';
+import { Table } from 'material-ui/Table';
+import Spinner from '../Spinner';
+configure({adapter: new Adapter()});
 
-// describe('<RepoTable />', () =>{
-//   const dataToTest = [
-//         {name: "repo",  forks: 2 },
-//         {name: "repo",  forks: 5},
-//         {name: "repo",  forks: 3},
-//         {name: "repo",  forks: 20},
-//   ];
+describe('<DayTable />', () =>{
+  const dataToTest = [
+        {Date: "2020-05-10T07:00:00+02:00", Temperature: {Minimun:{Value:40}} },
+        {Date: "2020-05-10T07:00:00+02:00",  Temperature: {Minimun:{Value:40}} },
+        {Date: "2020-05-10T07:00:00+02:00",  Temperature: {Minimun:{Value:40}} },
+        {Date: "2020-05-10T07:00:00+02:00",  Temperature: {Minimun:{Value:40}} },
+  ];
 
-//   it('should render spinner if data is loading', () =>{
-//     const wrapper = shallow(<RepoTable loading/>);
-//     expect(wrapper.find(Spinner)).toHaveLength(1);
-//   })
+  it('should render spinner if data is loading', () =>{
+    const wrapper = shallow(<DayTable loading/>);
+    expect(wrapper.find(Spinner)).toHaveLength(1);
+  })
 
-//   it('should render table when receiving reposData', () =>{
-//     const wrapper = shallow(<RepoTable loading={false} error ={false}
-//       repoData = {dataToTest}/>);
-//     expect(wrapper.find(Table)).toHaveLength(1);
-//   })
+  it('should render table when receiving reposData', () =>{
+    const wrapper = shallow(<DayTable loading={false} error ={false}
+    dayForecastData = {dataToTest}/>);
+    expect(wrapper.find(Table)).toHaveLength(1);
+  })
 
-// });
+});
--- weather-webapp/src/components/HourTable/HourTable.test.js
+++ weather-webapp/src/components/HourTable/HourTable.test.js
@@ -3,25 +3,25 @@
 // import Adapter from 'enzyme-adapter-react-16';
 // import CommitsTable from './CommitsTable';
 // import {List, ListItem} from 'material-ui/List';
-// import Spinner from '../Spinner';
-// configure({adapter: new Adapter()});
+import Spinner from '../Spinner';
+configure({adapter: new Adapter()});
 
-// describe('<RepoTable />', () =>{
-//   const dataToTest = [
-//         {author: {avatar_url:'someUrl'}, commit:{message:'someMsg', author:{name:'Author'}} },
-//         {author: {avatar_url:'someUrl'}, commit:{message:'someMsg', author:{name:'Author'}} },
-//         {author: {avatar_url:'someUrl'}, commit:{message:'someMsg', author:{name:'Author'}} }
-//   ];
+describe('<HourTable />', () =>{
+  const dataToTest = [
+        { Hour: 9, Temperature: {Minimun:{Value:40}}},
+        { Hour: 9, Temperature: {Minimun:{Value:40}}},
+        { Hour: 9, Temperature: {Minimun:{Value:40}}} 
+  ];
 
-//   it('should render spinner if data is loading', () =>{
-//     const wrapper = shallow(<CommitsTable loadingCommits/>);
-//     expect(wrapper.find(Spinner)).toHaveLength(1);
-//   })
+  it('should render spinner if data is loading', () =>{
+    const wrapper = shallow(<HourTable loadingHourForecast/>);
+    expect(wrapper.find(Spinner)).toHaveLength(1);
+  })
 
-//   it('should render List when receiving commitsData', () =>{
-//     const wrapper = shallow(<CommitsTable loadingCommits={false} errorCommits ={false}
-//       commitsData = {dataToTest}/>);
-//     expect(wrapper.find(List)).toHaveLength(1);
-//   })
+  it('should render List when receiving commitsData', () =>{
+    const wrapper = shallow(<HourTable loadingHourForecast={false} errorHourForecast ={false}
+        hourForecastData = {dataToTest}/>);
+    expect(wrapper.find(List)).toHaveLength(1);
+  })
 
-// });
+});
