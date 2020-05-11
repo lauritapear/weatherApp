1e1fa7caee880dc0e3c7d413d77eac4589ce26f2
Laura Artunduaga <laura.perea.artunduaga@gmail.com>
2020-05-10 22:45:29 -0400

    adding docker compose

--- weather-service/src/components/forecastService/index.ts
+++ weather-service/src/components/forecastService/index.ts
@@ -6,7 +6,7 @@ export async function getCityCode(req: Request, res: Response, next: NextFunctio
   const forecastService = ForecastService.getInstance();
   try {
     console.log('Request query: ', req.query.cityName);
-    let code = await forecastService.cityCode(req.query.cityName);
+    let code = await forecastService.cityCode(req.query.cityName.toString());
     res.send('city code is: ' + code);
   } catch (error) {
     console.log(`is instanceof HttpError ${error instanceof HttpError}`);
@@ -18,7 +18,7 @@ export async function getCityCode(req: Request, res: Response, next: NextFunctio
 export async function getSingleDay(req: Request, res: Response, next: NextFunction): Promise<void> {
   const forecastService = ForecastService.getInstance();
   try {
-    let cityCode = await forecastService.cityCode(req.query.cityName);
+    let cityCode = await forecastService.cityCode(req.query.cityName.toString());
     let singleDayForecast = await forecastService.oneDayForecast(cityCode);
     res.send('singleDayForecast: ' + singleDayForecast);
   } catch (error) {
@@ -31,7 +31,7 @@ export async function getSingleDay(req: Request, res: Response, next: NextFuncti
 export async function getFiveDays(req: Request, res: Response, next: NextFunction): Promise<void> {
   const forecastService = ForecastService.getInstance();
   try {
-    let cityCode = await forecastService.cityCode(req.query.cityName);
+    let cityCode = await forecastService.cityCode(req.query.cityName.toString());
     let fiveDaysForecast = await forecastService.fiveDaysForecast(cityCode);
     res.send(fiveDaysForecast);
   } catch (error) {
@@ -44,7 +44,7 @@ export async function getFiveDays(req: Request, res: Response, next: NextFunctio
 export async function getTwelveHours(req: Request, res: Response, next: NextFunction): Promise<void> {
   const forecastService = ForecastService.getInstance();
   try {
-    let cityCode = await forecastService.cityCode(req.query.cityName);
+    let cityCode = await forecastService.cityCode(req.query.cityName.toString());
     let twelveHoursForecast = await forecastService.twelveHoursForecast(cityCode);
     res.send(twelveHoursForecast);
   } catch (error) {
